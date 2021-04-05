4dd4961d1227b44343931e856cee07584a375c0e
Vlad Kokhan <vvlad0202@gmail.com>
2021-04-05 15:36:08 +0300

    refactor: task solution

--- Array/Plus-One.js
+++ Array/Plus-One.js
@@ -5,15 +5,17 @@
  */
 const plusOne = (digits) => {
   for (let i = digits.length - 1; i >= 0; i--) {
-    if (i === 0 && digits[i] === 9) {
-      digits[i] = 0;
-      digits.unshift(1);
-    } else if (digits[i] === 9) {
+    if (digits[i] === 9) {
       digits[i] = 0;
     } else {
       digits[i] = digits[i] + 1;
       break;
     }
   }
+
+  if (digits[0] === 0) {
+    digits.unshift(1);
+  }
+
   return digits;
 };
