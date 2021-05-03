1de8973c2a5138da77620c6eb30b5ae8e2d2bbab
Vlad Kokhan <vvlad0202@gmail.com>
2021-05-03 16:14:15 +0300

    feature: add task solution

---
+++ Tree/Invert-Binary-Tree.js
@@ -0,0 +1,22 @@
+/**
+ * Easy - https://leetcode.com/problems/invert-binary-tree/
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+const invertTree = (root) => {
+  if (!root) return null;
+
+  const tempLeft = root.left;
+  root.left = invertTree(root.right);
+  root.right = invertTree(tempLeft);
+
+  return root;
+};
