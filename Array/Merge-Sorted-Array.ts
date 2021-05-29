e131d1f91ae46b3348d5a897a9e87e8cfbdfcfee
Vlad Kokhan <vvlad0202@gmail.com>
2021-05-29 10:56:46 +0300

    feature: add task solution

---
+++ Array/Merge-Sorted-Array.ts
@@ -0,0 +1,13 @@
+/**
+ * Easy - https://leetcode.com/problems/merge-sorted-array/
+ * Do not return anything, modify nums1 in-place instead.
+ */
+const merge = (
+  nums1: number[],
+  m: number,
+  nums2: number[],
+  n: number,
+): void => {
+  nums1.splice(m, n, ...nums2);
+  nums1.sort((a, b) => a - b);
+};
