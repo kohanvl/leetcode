c77f29c30a645a14f3ece2457dccc541313617c7
Vlad Kokhan <vvlad0202@gmail.com>
2021-06-07 08:33:52 +0300

    refactor: add type fo hashTable

--- Hash-Table/Find-the-Duplicate-Number.ts
+++ Hash-Table/Find-the-Duplicate-Number.ts
@@ -1,6 +1,6 @@
 // Medium - https://leetcode.com/problems/find-the-duplicate-number/
 const findDuplicate = (nums: number[]): number => {
-  const hashTable = {};
+  const hashTable: {[key: string]: number} = {};
   for (let i = 0; i < nums.length; i++) {
     hashTable[nums[i]] = hashTable[nums[i]] + 1 || 1;
     if (hashTable[nums[i]] === 2) return nums[i];
