f79c1d4867f4af8c4bd85fcc3937362b3a424b03
Vlad Kokhan <vvlad0202@gmail.com>
2021-06-23 14:25:54 +0300

    feature: add task solution

---
+++ Tree/Validate-Binary-Search-Tree.ts
@@ -0,0 +1,25 @@
+/**
+ * Medium - https://leetcode.com/problems/validate-binary-search-tree/
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+const isValidBST = (
+  root: TreeNode | null,
+  min: TreeNode | null = null,
+  max: TreeNode | null = null,
+): boolean => {
+  if (!root) return true;
+  if (min && root.val <= min.val) return false;
+  if (max && root.val >= max.val) return false;
+  return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+};
