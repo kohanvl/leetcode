8e123d3cdcfb871c9a06e48ba368a94be69cc41e
Vlad Kokhan <vvlad0202@gmail.com>
2021-08-11 12:16:16 +0300

    feature: add task solution

---
+++ Array/Max-Consecutive-Ones-Solution.ts
@@ -0,0 +1,10 @@
+// Easy - https://leetcode.com/problems/max-consecutive-ones/
+function findMaxConsecutiveOnes(nums: number[]): number {
+  let max = 0;
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    count = nums[i] === 1 ? count + 1 : 0;
+    max = Math.max(max, count);
+  }
+  return max;
+}
