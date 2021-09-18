491b34ae6afb29acfa06885d437c6eaa01d23be8
Vlad Kokhan <vvladd@users.noreply.github.com>
2021-09-18 08:16:10 +0300

    refactor: from js to ts

--- String/Reverse-String.ts
+++ String/Reverse-String.ts
@@ -1,9 +1,8 @@
 /**
  * Easy - https://leetcode.com/problems/reverse-string/
- * @param {character[]} s
- * @return {void} Do not return anything, modify s in-place instead.
+ * Do not return anything, modify s in-place instead.
  */
-const reverseString = (s) => {
+const reverseString = (s: string[]): void => {
   for (let i = 0; i < s.length; i++) {
     if (Math.floor(s.length / 2) === i) break;
     const temp = s[s.length - i - 1];
