eb752effaccb1f66ff279211e3e2a93803b44ca7
Vlad Kokhan <vvlad0202@gmail.com>
2021-10-18 15:21:16 +0300

    refactor: from js to ts

---
+++ Array/How-Many-Numbers-Are-Smaller-Than-the-Current-Number.ts
@@ -0,0 +1,12 @@
+// Easy - https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+const smallerNumbersThanCurrent = (nums: number[]): number[] => {
+  const result: number[] = [];
+  for (let i = 0; i < nums.length; i++) {
+    let count = 0;
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[i] > nums[j]) count++;
+    }
+    result.push(count);
+  }
+  return result;
+};
