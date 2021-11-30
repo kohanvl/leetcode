a56016781de6766872cefb6393b4b89ec1adb26d
Vlad Kokhan <vvlad0202@gmail.com>
2021-11-30 20:44:52 +0300

    refactor: from js to ts

---
+++ Array/Running-Sum-of-1d-Array.ts
@@ -0,0 +1,7 @@
+// Easy - https://leetcode.com/problems/running-sum-of-1d-array/
+const runningSum = (nums: number[]): number[] => {
+  for (let i = 1; i < nums.length; i++) {
+    nums[i] += nums[i - 1];
+  }
+  return nums;
+};
