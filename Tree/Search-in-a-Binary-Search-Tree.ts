dc5f9f335868724509dd5cf40937c114897086f5
Vlad Kokhan <vvlad0202@gmail.com>
2021-12-01 08:32:10 +0300

    refactor: from js to ts

---
+++ Tree/Search-in-a-Binary-Search-Tree.ts
@@ -0,0 +1,21 @@
+/**
+ * Easy - https://leetcode.com/problems/search-in-a-binary-search-tree/
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+const searchBST = (root: TreeNode | null, val: number): TreeNode | null => {
+  if (!root) return null;
+  else if (root.val === val) return root;
+  else if (root.val > val) return searchBST(root.left, val);
+  else return searchBST(root.right, val);
+};
