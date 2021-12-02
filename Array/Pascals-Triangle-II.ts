368be2386fac144b9ba9057c5ce2135538edcc0b
Vlad Kokhan <vvlad0202@gmail.com>
2021-12-02 08:50:23 +0300

    feature: add Pascal's Triangle II solution

---
+++ Array/Pascals-Triangle-II.ts
@@ -0,0 +1,16 @@
+//Easy - https://leetcode.com/problems/pascals-triangle-ii/
+const getRow = (rowIndex: number): number[] => {
+  const triangle = [[1]];
+
+  for (let i = 1; i <= rowIndex; i++) {
+    const row: number[] = [];
+    row.push(1);
+    for (let j = 0; j < triangle[i - 1].length - 1; j++) {
+      row.push(triangle[i - 1][j] + triangle[i - 1][j + 1]);
+    }
+    triangle.push(row);
+    row.push(1);
+  }
+  console.log(triangle);
+  return triangle[rowIndex];
+};
