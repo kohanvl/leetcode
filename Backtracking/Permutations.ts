c43c1aee8c5550b68f7abd5b36b63389107f3c10
Vlad Kokhan <vvlad0202@gmail.com>
2022-02-23 17:32:11 +0300

    refactor: permute

--- Backtracking/Permutations.ts
+++ Backtracking/Permutations.ts
@@ -1,16 +1,16 @@
-const permute = (
-  nums: number[],
-  set: number[] = [],
-  ans: number[][] = [],
-): number[][] => {
-  if (!nums.length) ans.push([...set]);
+// Medium - https://leetcode.com/problems/permutations/
+// [1,2,3] -> [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+function permute(nums: number[]): number[][] {
+  return helper(nums, [], []);
+}
 
+function helper(nums: number[], arr: number[], ans: number[][]): number[][] {
+  if (!nums.length) ans.push([...arr]);
   for (let i = 0; i < nums.length; i++) {
-    const fNums = nums.filter((_, ind) => ind !== i);
-    set.push(nums[i]);
-    permute(fNums, set, ans);
-    set.pop();
+    const fNums = nums.filter((_, ind) => ind !== i); // exclude current number
+    arr.push(nums[i]);
+    helper(fNums, arr, ans);
+    arr.pop();
   }
-
   return ans;
-};
+}
