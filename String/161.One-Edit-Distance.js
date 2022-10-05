74b81ca137a0965984bf592430ad374b9ed23965
Vlad Kokhan <vvlad0202@gmail.com>
2022-10-05 20:14:58 +0300

    feature: add solution for 161. One Edit Distance task

---
+++ String/161.One-Edit-Distance.js
@@ -0,0 +1,52 @@
+/**
+PREMIUM
+Medium - https://leetcode.com/problems/one-edit-distance/
+
+Given two strings s and t, determine if they are both one edit distance apart.
+
+Note: 
+There are 3 possiblities to satisify one edit distance apart:
+Insert a character into s to get t
+Delete a character from s to get t
+Replace a character of s to get t
+
+Example:
+Input: s = "ab", t = "acb"
+Output: true
+*/
+
+const isOneEditDistance = (s, t) => {
+  const sLen = s.length;
+  const tLen = t.length;
+  let i = 0;
+  let j = 0;
+  let count = 0;
+  if (Math.abs(sLen - tLen) > 1) return false;
+
+  while (i < sLen && j < tLen) {
+    if (s[i] !== t[j]) {
+      if (count === 1) return false;
+      else if (sLen > tLen) i++;
+      else if (sLen < tLen) j++;
+      else {
+        i++;
+        j++;
+      }
+      count++;
+    } else {
+      i++;
+      j++;
+    }
+  }
+
+  if (i < sLen && j < tLen) count++;
+
+  return count === 1;
+};
+
+console.log(isOneEditDistance('ab', 'acb')); // true
+console.log(isOneEditDistance('fa', 'a')); // true
+console.log(isOneEditDistance('1203', '1213')); // true
+console.log(isOneEditDistance('cab', 'ad')); // false
+console.log(isOneEditDistance('aa', 'aa')); //false
+console.log(isOneEditDistance('', '')); //false
