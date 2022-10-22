8a5e51228601d7aa06815a74ba4c48bae11b7b45
Vlad Kokhan <vvlad0202@gmail.com>
2022-10-22 09:32:50 +0400

    feature: add 103. Binary Tree Zigzag Level Order Traversal solution

---
+++ Tree/103.Binary-Tree-Zigzag-Level-Order-Traversal.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+// Use BFS
+var zigzagLevelOrder = function (root) {
+  if (!root) return [];
+  const queue = [root];
+  const result = [];
+  let deep = 0;
+  while (queue.length) {
+    const qLen = queue.length;
+    const level = [];
+    for (let i = 0; i < qLen; i++) {
+      const node = queue.shift();
+      // check deep level and puhs or unshift to level arr
+      if (deep % 2 === 0) level.push(node.val);
+      else level.unshift(node.val);
+      //check childs
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    // push level arr to result arr
+    result.push(level);
+    deep++;
+  }
+  return result;
+};
