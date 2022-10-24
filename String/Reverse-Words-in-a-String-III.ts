fd368181656c65e094f85f5403a997d1d86df044
Vlad Kokhan <vvlad0202@gmail.com>
2022-10-24 18:21:56 +0400

    refactor: reverse-word-3 task

--- String/Reverse-Words-in-a-String-III.ts
+++
@@ -1,7 +0,0 @@
-// Easy - https://leetcode.com/problems/reverse-words-in-a-string-iii/
-const reverseWords = (s: string): string => {
-  return s
-    .split(' ')
-    .map((word) => word.split('').reverse().join(''))
-    .join(' ');
-};
